a4e94d2e00df68fcd7a904cc58d41f466da0b831
Sergey Vakhramov <vakhramoff@xakep.ru>
2020-04-17 23:05:15 +0500

    Changed change detection strategy in main-layout.component.

--- src/app/shared/reusable-components/main-layout/main-layout.component.ts
+++ src/app/shared/reusable-components/main-layout/main-layout.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainLayoutComponent implements OnInit {
   @Input() title = 'COVID';
