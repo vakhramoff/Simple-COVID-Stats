14584683c5fcc2bafee4cc68e3b73f32f98f1959
Sergey Vakhramov <vakhramoff@xakep.ru>
2020-04-19 09:05:37 +0500

    Fixed layout.

--- src/app/country-page/country-page.component.ts
+++ src/app/country-page/country-page.component.ts
@@ -1,27 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 import { CovidStatsService } from '../shared/covid-stats.service';
 import { CountryStatisticInfo } from '../shared/interfaces/country-statistic-info';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styleUrls: ['./country-page.component.sass'],
+  providers: [UpperCasePipe],
 })
 export class CountryPageComponent implements OnInit {
   public countryStat$: Observable<CountryStatisticInfo>;
-  public countryCode: string;
 
-  constructor(private covidStatsService: CovidStatsService, private route: ActivatedRoute) {}
+  private countryCode: string;
+  private countryName: string;
+
+  constructor(private covidStatsService: CovidStatsService, private route: ActivatedRoute, private upperCasePipe: UpperCasePipe) {}
 
   ngOnInit(): void {
     this.countryStat$ = this.route.params.pipe(
       switchMap((params: Params) => {
-        this.countryCode = params.countryCode;
-        return this.covidStatsService.getByCountry(params.countryCode);
+        this.countryCode = this.upperCasePipe.transform(params.countryCode);
+        return this.covidStatsService.getByCountry(params.countryCode).pipe(
+          tap((countryData: CountryStatisticInfo) => {
+            this.countryName = countryData.country;
+          }),
+        );
       }),
     );
+  }
+
+  get headerTitle() {
+    return `COVID${this.countryCode && this.countryName ? ` / ${this.countryName}, ${this.countryCode}` : ''}`;
   }
 }
