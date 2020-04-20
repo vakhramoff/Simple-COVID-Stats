9d6dcbe919b10784c72dca00bf0c42632e6b6c31
Sergey Vakhramov <vakhramoff@xakep.ru>
2020-04-20 15:28:49 +0500

    Install transloco.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { CountryPageComponent } from './country-page/country-page.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco-root.module';
 
 registerLocaleData(localeRussian, 'ru');
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent, CountryPageComponent, ErrorPageComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, SharedModule, HttpClientModule, TranslocoRootModule],
   providers: [],
   bootstrap: [AppComponent],
 })
