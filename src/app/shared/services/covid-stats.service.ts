93be32ff82e63f93569bb9aff650425809fe61ec
Sergey Vakhramov <vakhramoff@xakep.ru>
2021-02-07 18:44:36 +0500

    Use decorator to catch HTTP errors.

--- src/app/shared/services/covid-stats.service.ts
+++ src/app/shared/services/covid-stats.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HandleHttpErrors } from '@vakhramoff/angular-utils';
 import { Observable } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 import { AllStatisticInfo } from '../interfaces/all-statistic-info';
 import { CountryStatisticInfo } from '../interfaces/country-statistic-info';
@@ -12,28 +13,20 @@ import { CountryStatisticInfo } from '../interfaces/country-statistic-info';
 export class CovidStatsService {
   constructor(private http: HttpClient) {}
 
+  @HandleHttpErrors()
   public getAll(): Observable<AllStatisticInfo> {
     return this.http.get<AllStatisticInfo>(`${environment.covidApiUrl}/all`).pipe(
       tap((data) => {
         console.info('General statistics', data);
       }),
-      catchError((error) => {
-        console.warn('An error occurred while loading general statistics', error);
-
-        throw error;
-      }),
     );
   }
 
+  @HandleHttpErrors()
   public getByCountry(countryCode: string): Observable<CountryStatisticInfo> {
     return this.http.get<CountryStatisticInfo>(`${environment.covidApiUrl}/countries/${countryCode}`).pipe(
       tap((data) => {
         console.info(`Statistics by country: ${countryCode}`, data);
-      }),
-      catchError((error) => {
-        console.warn(`An error occurred while loading statistics for country: ${countryCode}`, error);
-
-        throw error;
       }),
     );
   }
