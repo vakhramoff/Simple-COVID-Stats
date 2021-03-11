5454a22a51fa3777bf701607e34da586e284e86e
Sergey Vakhramov <vakhramoff@xakep.ru>
2021-03-11 11:02:52 +0500

    (feature) Switch to Angular Utils' toLocaleString pipe.

--- src/app/shared/pipes/to-locale-string.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToLocaleStringPipe } from './to-locale-string.pipe';
-
-describe('ToLocaleStringPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToLocaleStringPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
