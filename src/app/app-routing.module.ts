38e3593258d329e05ba1990223a99daf6d009b9d
Sergey Vakhramov <vakhramoff@xakep.ru>
2022-06-11 21:14:20 +0500

    feat: update angular to 14 version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CountryPageComponent } from './country-page/country-page.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'country/:countryCode', component: CountryPageComponent },
-  { path: '**', component: ErrorPageComponent },
+  { path: '**', loadComponent: () => import('./error-page/error-page.component').then(({ ErrorPageComponent }) => ErrorPageComponent) },
 ];
 
 @NgModule({
