25c6cc565c2a5ea67b53d4719b2d42a10612e9f2
Sergey Vakhramov <vakhramoff@xakep.ru>
2022-06-13 11:10:37 +0500

    feat: update angular-utils

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
-import { PipesModule } from '@vakhramoff/angular-utils';
+import { ToLocaleStringPipe } from '@vakhramoff/angular-utils';
 import { Observable } from 'rxjs';
 import { AllStatisticInfo, CovidStatsService } from '../../data-access';
 import { DataCardComponent, MainLayoutComponent } from '../../layout';
@@ -12,7 +12,7 @@ import { DataCardComponent, MainLayoutComponent } from '../../layout';
   styleUrls: ['./home-page.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule, DataCardComponent, MainLayoutComponent, TranslocoModule, PipesModule],
+  imports: [CommonModule, DataCardComponent, MainLayoutComponent, ToLocaleStringPipe, TranslocoModule],
 })
 export class HomePageComponent {
   readonly allStats$: Observable<AllStatisticInfo> = this.covidStatsService.getAll();
